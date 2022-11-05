96b90e6d7db00f55a7fa830452adcbfb6fa44eea
jiri.one <nemec@jiri.one>
2022-11-05 14:07:12 +0100

    first async view

---
+++ uzlatekachny/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index_cze, name='index_cze'),
+    # path('cze', views.index_cze, name='index_cze'),
+    # path('eng', views.index_eng, name='index_eng'),
+    # path('search', views.search, name='search'),
+]
--- uzlatekachny/views.py
+++ uzlatekachny/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+async def index_cze(request):
+    return render(request, 'index.html')
