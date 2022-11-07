24083d4a29a66370cf39fa3f8859ecf66846da5d
jiri.one <jiri1@duck.com>
2022-11-07 11:33:31 +0100

    first working model

--- uzlatekachny/admin.py
+++ uzlatekachny/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Food
+
+for model in [Food]:
+    admin.site.register(model)
--- uzlatekachny/models.py
+++ uzlatekachny/models.py
@@ -1,6 +1,11 @@
 from django.db import models
+from djmoney.models.fields import MoneyField
 
 class Food(models.Model):
     name = models.CharField("Food name", unique=True, max_length=100)
-    price = models.
+    price = MoneyField(max_digits=14, decimal_places=2, default_currency='EUR')
+    ingredients = models.TextField("Food ingredients", blank=True)
+    notes = models.TextField("Food notes", blank=True)
     
+    def __str__(self):
+        return self.name
