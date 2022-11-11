591d6da5e609d6888a5b20fee8b06aa399525aba
jiri.one <nemec@jiri.one>
2022-11-11 11:14:26 +0100

    removed unnecessarily (un)register

--- uzlatekachny/admin.py
+++ uzlatekachny/admin.py
@@ -3,17 +3,11 @@ from modeltranslation.admin import TranslationAdmin
 
 from .models import Food, Category
 
-for model in [Food, Category]:
-    admin.site.register(model)
-
 class FoodAdmin(TranslationAdmin):
     pass
     
-
 class CategoryAdmin(TranslationAdmin):
     pass
 
-admin.site.unregister(Food)
-admin.site.unregister(Category)
 admin.site.register(Food, FoodAdmin)
 admin.site.register(Category, CategoryAdmin)
