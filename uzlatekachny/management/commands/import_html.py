5e83858f71011a8a6ec4b3f3dbe2704509a9427c
jiri.one <nemec@jiri.one>
2022-11-11 10:55:39 +0100

    remove comment

--- uzlatekachny/management/commands/import_html.py
+++ uzlatekachny/management/commands/import_html.py
@@ -72,35 +72,6 @@ class Command(BaseCommand):
                         food.ingredients_ru = ingredience_rus
                         food.ingredients_de = ingredience_ger
                         food.save()
-                    
-                
-                
-
-
-        
-        
-        # for post in Post.objects.all():
-            
-        #     for tag in soup.find_all():
-        #         if 'src' in tag.attrs:
-        #             if 'soubory' in tag['src']:
-        #                 index = tag['src'].index('soubory') + 7 # end of soubory
-        #                 tag['src'] = f"/files{tag['src'][index:]}"
-        #         if 'href' in tag.attrs:
-        #             if 'soubory' in tag['href']:
-        #                 index = tag['href'].index('soubory') + 7 # end of soubory
-        #                 tag['href'] = f"/files{tag['href'][index:]}"
-        #         if 'poster' in tag.attrs:
-        #             if 'soubory' in tag['poster']:
-        #                 index = tag['poster'].index('soubory') + 7 # end of soubory
-        #                 tag['poster'] = f"/files{tag['poster'][index:]}"
-            
-        #     soup = str(soup)
-        #     if 'soubory/' in soup:
-        #         soup = soup.replace('soubory/', 'files/')
-
-        #     post.content_cze = soup
-        #     post.save()
     
     def add_arguments(self, parser):
         parser.add_argument('html_menu', nargs="?", default="menu.htm")
