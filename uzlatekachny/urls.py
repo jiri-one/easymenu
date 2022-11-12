196ef887686db1c733928185b351e859a897b619
jiri.one <nemec@jiri.one>
2022-11-12 09:55:12 +0100

    first version with template

--- uzlatekachny/urls.py
+++ uzlatekachny/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index_cze, name='index_cze'),
+    path('', views.FoodListView.as_view()),
     # path('cze', views.index_cze, name='index_cze'),
     # path('eng', views.index_eng, name='index_eng'),
     # path('search', views.search, name='search'),
--- uzlatekachny/views.py
+++ uzlatekachny/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.http import HttpResponse
+from uzlatekachny.models import Food, Category
 
-# Create your views here.
-async def index_cze(request):
-    return render(request, 'index.html')
+class FoodListView(ListView):
+    """Main class to show food list."""
+    model = Food
+    template_name = 'index.html'
+    
+    # async def get(self, request, *args, **kwargs):
+    #     return HttpResponse("Hello async world!")
