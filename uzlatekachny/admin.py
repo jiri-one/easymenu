d68e903f709cd78fab0509da7ef24306f3775364
jiri.one <nemec@jiri.one>
2022-11-15 09:32:28 +0100

    improved admin forms

--- uzlatekachny/admin.py
+++ uzlatekachny/admin.py
@@ -5,7 +5,20 @@ from .models import Food, Category
 
 @admin.register(Food)
 class FoodAdmin(admin.ModelAdmin):
-    exclude = ['name', 'name_i18n']
+    fieldsets = (
+        (None, {
+            'fields': ('name_cs', 'name_en', 'name_de', 'name_ru')
+        }),
+        ('Add ingredients', {
+            'classes': ('collapse',),
+            'fields': ('ingredients_cs', 'ingredients_en', 'ingredients_de', 'ingredients_ru'),
+        }),
+        ('Add notes', {
+            'classes': ('collapse',),
+            'fields': ('notes_cs', 'notes_en', 'notes_de', 'notes_ru'),
+        })
+    )
+    #exclude = ['name', 'name_i18n', 'ingredients', 'ingredients_i18n','notes', 'notes_i18n']
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
