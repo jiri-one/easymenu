c11c2cb68380b62c159ee1cc8fcfa4b8dae56c8c
jiri.one <nemec@jiri.one>
2022-11-21 15:45:53 +0100

    finished changing order

--- uzlatekachny/models.py
+++ uzlatekachny/models.py
@@ -12,7 +12,7 @@ class Food(models.Model):
         return Food.objects.last().order + 1
     
     def next_id(): # type: ignore
-        return Food.objects.last().id + 1
+        return Food.objects.count() + 1
     
     @staticmethod
     def refresh_order(): # type: ignore
@@ -32,12 +32,14 @@ class Food(models.Model):
     notes = models.CharField("Food notes", blank=True, null=True, max_length=200)
     category = models.ForeignKey("Category", on_delete=models.CASCADE)
     order = models.IntegerField(unique=False, default=next_order_nr)
-    add_time = models.DateTimeField(auto_now=True)
+    change_time = models.DateTimeField(auto_now=True)
     i18n = TranslationField(fields=("name", "ingredients", "notes"))
     
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self._prev_order = self.order
+        if hasattr(self, "category"):
+            self._prev_category = self.category
     
     def __str__(self):
         return self.name
@@ -45,11 +47,15 @@ class Food(models.Model):
     def clean(self, *args, **kwargs):
         if hasattr(self, "category") and hasattr(self, "order"):
             cat_values = list(self.category.food_set.values_list("order", flat=True))
-            if not (cat_values[0] <= self.order <= cat_values[-1] + 1):
-                raise ValidationError({'order': f"Číslo musí být mezi {cat_values[0]} a {cat_values[-1] + 1} nebo zvolte jinou kategorii!"})
+            new_in_cat = 1
+            if hasattr(self, "_prev_category"):
+                if self._prev_category == self.category:
+                    new_in_cat = 0 # I don't need to add next order nr. if only move food in the same category
+            if not (cat_values[0] <= self.order <= cat_values[-1] + new_in_cat):
+                raise ValidationError({'order': f"Číslo musí být mezi {cat_values[0]} a {cat_values[-1] + new_in_cat} nebo zvolte jinou kategorii!"})
            
     class Meta:
-        ordering = ["order", "-add_time"]
+        ordering = ["order", "-change_time"]
     
 class Category(models.Model):
     def get_next_id(): # type: ignore
@@ -68,12 +74,19 @@ class Category(models.Model):
 def change_order_if_save_same(sender, instance, **kwargs):
     same = sender.objects.filter(order=instance.order).count()
     if same > 1:
-        # if previous value of order is lower or higher, I can (and I have to) only switch the values
+        # if previous value of order is +-1, I can only switch the values
         if instance._prev_order in [instance.order+1, instance.order-1]:
             # get second food with same order number
             sec_food = sender.objects.filter(order=instance.order).exclude(pk=instance.pk)[0]
             # update second food order number to previous value from actual food
             sender.objects.filter(pk=sec_food.pk).update(order=instance._prev_order)
+        # if I change order to higher number (more in the list below) I have to +1,
+        # because the changed order is always first in the list (when two same order numbers)
+        # that's because of ordering = ["order", "-change_time"], -change_time is second ordering
+        elif (instance.order - instance._prev_order) >= 2:
+            sender.objects.filter(pk=instance.pk).update(order=instance.order+1)
+            sender.refresh_order()
+        # if I change order to lower number (higher in list), then it is OK and I can only refresh_order()
         else:
             sender.refresh_order()
 
